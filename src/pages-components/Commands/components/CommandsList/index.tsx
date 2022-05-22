40a9c1b692745cd72b587488b753cdbfc3612174
Bryan Martins <bryanmaraujo544@gmail.com>
2022-05-22 16:09:59 -0300

    feat: adding the table of commands list

--- src/pages-components/Commands/components/CommandsList/index.tsx
+++ src/pages-components/Commands/components/CommandsList/index.tsx
@@ -1,7 +1,46 @@
 import { CommandsListLayout } from './layout';
 
+const mockCommands = [
+  {
+    id: 2,
+    table: 'João Gomes',
+    waiter: 'Diego',
+    total: 458.9,
+  },
+  {
+    id: 3,
+    table: 'João Gomes',
+    waiter: 'Diego',
+    total: 458.9,
+  },
+  {
+    id: 4,
+    table: 'João Gomes',
+    waiter: 'Diego',
+    total: 458.9,
+  },
+  {
+    id: 5,
+    table: 'João Gomes',
+    waiter: 'Diego',
+    total: 458.9,
+  },
+  {
+    id: 6,
+    table: 'João Gomes',
+    waiter: 'Diego',
+    total: 458.9,
+  },
+  {
+    id: 7,
+    table: 'João Gomes',
+    waiter: 'Diego',
+    total: 458.9,
+  },
+];
+
 export const CommandsList = () => {
   console.log('commands list');
 
-  return <CommandsListLayout />;
+  return <CommandsListLayout items={mockCommands} />;
 };
--- src/pages-components/Commands/components/CommandsList/layout.tsx
+++ src/pages-components/Commands/components/CommandsList/layout.tsx
@@ -1,6 +1,69 @@
-import { Table } from '@chakra-ui/react';
+import {
+  Icon,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from '@chakra-ui/react';
+import { AiOutlineDelete } from 'react-icons/ai';
+import { BiAddToQueue } from 'react-icons/bi';
+import { CgOptions } from 'react-icons/cg';
+import { FiEdit2 } from 'react-icons/fi';
+import { formatDecimalNum } from 'utils/formatDecimalNum';
 
-export const CommandsListLayout = () => {
+const listColumns = ['Mesa', 'Garçom', 'Total', ''];
+
+type Props = {
+  items: any[];
+};
+
+export const CommandsListLayout = ({ items }: Props) => {
   console.log('commands layout');
-  return <Table>'table'</Table>;
+  return (
+    <TableContainer>
+      <Table>
+        <Thead>
+          <Tr>
+            {listColumns.map((listItem) => (
+              <Th key={`command-list-${listItem}`}>{listItem}</Th>
+            ))}
+          </Tr>
+        </Thead>
+        <Tbody>
+          {items.map(({ id, table, waiter, total }) => (
+            <Tr key={`list-command-${id}`} h={20}>
+              <Td>{table}</Td>
+              <Td>{waiter}</Td>
+              <Td>
+                R$ {formatDecimalNum({ num: total.toString(), to: 'comma' })}
+              </Td>
+              <Td>
+                <Menu>
+                  <MenuButton>
+                    <Icon as={CgOptions} fontSize={[14, 18]} color="blue.800" />
+                  </MenuButton>
+                  <MenuList>
+                    <MenuItem icon={<BiAddToQueue />}>
+                      Adicionar Produtos
+                    </MenuItem>
+                    <MenuItem icon={<FiEdit2 />}>Editar</MenuItem>
+                    <MenuItem icon={<AiOutlineDelete />} color="red.500">
+                      Deletar
+                    </MenuItem>
+                  </MenuList>
+                </Menu>
+              </Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </TableContainer>
+  );
 };
