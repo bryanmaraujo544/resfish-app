d4ed01fe47d68af18dd818b2384e936f505a62da
Bryan Martins <bryanmaraujo544@gmail.com>
2022-06-02 21:02:32 -0300

    fix: fixing url name

--- src/services/serverApi.ts
+++ src/services/serverApi.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const prodEnv = 'https://pesqueiro-arrudas.herokuapp.com';
+const url = 'https://pesqueiro-arrudas.herokuapp.com';
 
 export const serverApi = axios.create({
-  baseURL: prodEnv,
+  baseURL: url,
 });
