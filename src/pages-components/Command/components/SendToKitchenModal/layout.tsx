82043ce9f689e0879d47d8aeb5765d0373f2c6df
Bryan Martins <bryanmaraujo544@gmail.com>
2022-06-15 08:47:59 -0300

    feat(senttokitchenmodal): adding textarea to user put some observations when sending to kitche

--- src/pages-components/Command/components/SendToKitchenModal/layout.tsx
+++ src/pages-components/Command/components/SendToKitchenModal/layout.tsx
@@ -1,4 +1,4 @@
-import { Button, Flex } from '@chakra-ui/react';
+import { Button, Flex, Stack, Textarea, Text } from '@chakra-ui/react';
 import { Modal } from 'components/Modal';
 
 interface Props {
@@ -19,17 +19,21 @@ export const SendToKitchenModalLayout = ({
     onClose={() => handleCloseModal()}
     title="Mandar para Cozinha?"
   >
-    <Flex gap={2}>
-      <Button flex={1}>Cancelar</Button>
-      <Button
-        flex={1}
-        colorScheme="blue"
-        onClick={() => handleSendToKitchen()}
-        isLoading={isSending}
-        loadingText="Enviando"
-      >
-        Mandar
-      </Button>
-    </Flex>
+    <Stack>
+      <Text>Observações:</Text>
+      <Textarea placeholder="Ex: Coca Cola com gelo e limão" />
+      <Flex gap={2} mt={2}>
+        <Button flex={1}>Cancelar</Button>
+        <Button
+          flex={1}
+          colorScheme="blue"
+          onClick={() => handleSendToKitchen()}
+          isLoading={isSending}
+          loadingText="Enviando"
+        >
+          Mandar
+        </Button>
+      </Flex>
+    </Stack>
   </Modal>
 );
