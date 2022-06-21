ec45456bb881b2e93d4c3ac3b137a43bb090c829
Bryan Martins <bryanmaraujo544@gmail.com>
2022-06-21 19:59:46 -0300

    feat(home): updating cashiers when a new cashier is closed

--- src/pages-components/Home/components/ClosedCashiers/index.tsx
+++ src/pages-components/Home/components/ClosedCashiers/index.tsx
@@ -1,13 +1,16 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 import { Cashier } from 'types/Cashier';
 import CashierService from 'pages-components/Home/services/CashierService';
 import { useRouter } from 'next/router';
+import { SocketContext } from 'pages/_app';
+import { DateTime } from 'luxon';
 import { ClosedCashiersLayout } from './layout';
 
 export const ClosedCashiers = () => {
   const [allCashiers, setAllCashiers] = useState<Cashier[]>([]);
 
+  const { socket } = useContext(SocketContext);
   const router = useRouter();
 
   useEffect(() => {
@@ -15,6 +18,41 @@ export const ClosedCashiers = () => {
       const cashiers = await CashierService.getAll();
       setAllCashiers(cashiers);
     })();
+  }, []);
+
+  useEffect(() => {
+    socket.on('cashier-created', (newCashier: Cashier) => {
+      setAllCashiers((prevCashiers) => {
+        const cashierAlreadyExists = prevCashiers.find((prevCashier) => {
+          const prevDt = DateTime.fromISO(prevCashier.date).setLocale('pt-BR');
+          const newCashierDt = DateTime.fromISO(newCashier.date).setLocale(
+            'pt-BR'
+          );
+
+          if (
+            prevDt.day === newCashierDt.day &&
+            prevDt.month === newCashierDt.month &&
+            prevDt.year === newCashierDt.year
+          ) {
+            return true;
+          }
+          return false;
+        });
+
+        if (cashierAlreadyExists) {
+          const newCashiers = prevCashiers.filter(
+            (prevCashier) => prevCashier._id !== cashierAlreadyExists._id
+          );
+          return [...newCashiers, newCashier];
+        }
+
+        return [...prevCashiers, newCashier];
+      });
+    });
+
+    return () => {
+      socket.off('cashier-created');
+    };
   }, []);
 
   function handleGoToCashierPage(cashierId: string) {
