6bf645d2b9c337dbcbb1e9335dfb67c8e2acd3b3
Bryan Martins <bryanmaraujo544@gmail.com>
2022-06-21 21:10:05 -0300

    fix: fixing import

--- src/pages-components/Stock/index.tsx
+++ src/pages-components/Stock/index.tsx
@@ -7,9 +7,11 @@ import {
   useReducer,
   useState,
   useEffect,
+  useContext,
 } from 'react';
 
 import { useRouter } from 'next/router';
+import { SocketContext } from 'pages/_app';
 import StockService from './services/index';
 import { StockLayout } from './layout';
 import { AddItemModal } from './components/AddItemModal';
