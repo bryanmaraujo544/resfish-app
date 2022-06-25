711dc67f41c7fcb266d38b8a56ba09d86a746bc3
Bryan Martins <bryanmaraujo544@gmail.com>
2022-06-25 18:02:42 -0300

    fix: general fixes
    Fixing the <p> nesting

--- src/pages-components/Command/index.tsx
+++ src/pages-components/Command/index.tsx
@@ -148,7 +148,7 @@ export const Command = ({ commandId }: Props) => {
       if (deletedCommandId === commandId) {
         toast.closeAll();
         toast({
-          status: 'error',
+          status: 'info',
           title: 'Comanda deletada',
           duration: 1000,
           isClosable: true,
